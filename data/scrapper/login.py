5cf933e504b2f127273be52e8309c5c373451f94
Ayushkaushal <ayushk4@gmail.com>
2018-10-17 09:39:48 +0530

    Add script for getting session cookie

--- data/scrapper/login.py
+++ data/scrapper/login.py
@@ -8,14 +8,28 @@ ERP_LOGIN_URL = 'https://erp.iitkgp.ac.in/IIT_ERP3/' #to get the session cookie
 SECURITY_QUESTION_URL = 'https://erp.iitkgp.ac.in/SSOAdministration/getSecurityQues.htm'
 
 
-while (1):
-    roll_no = input("Enter your roll number : ")
-    r2 = requests.post (SECURITY_QUESTION_URL, data = {'user_id': roll_no})
-    if(r2.text!= 'FALSE'):
-        break
-    print("Roll number is wrong\nPlease Enter the details again\n")
-    time.sleep(1)
+def get_session_cookie ():
+    request_erp = requests.get(ERP_LOGIN_URL)
+    
+    #Following two lines are needed coz erp has freaking two html tags
+    soup = BeautifulSoup(request_erp.text)
+    soup = BeautifulSoup(request_erp.text[len(soup):])
 
-password = getpass.getpass("Enter your password : ")
-security_ans = getpass.getpass("Answer your  security question - " + r2.text + " : ")
+    session_tag = soup.find('input',attrs = {'id':'sessionToken'})
+    session_cookie = session_tag['value']
+    return session_cookie
+
+print(get_session_cookie())
+
+def get_security():
+    while (1):
+        roll_no = input("Enter your roll number : ")
+        r2 = requests.post (SECURITY_QUESTION_URL, data = {'user_id': roll_no})
+        if(r2.text!= 'FALSE'):
+            break
+        print("Roll number is wrong\nPlease Enter the details again\n")
+        time.sleep(1)
+
+    password = getpass.getpass("Enter your password : ")
+    security_ans = getpass.getpass("Answer your  security question - " + r2.text + " : ")
 
