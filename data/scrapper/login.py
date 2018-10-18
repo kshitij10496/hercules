61017fb11760162135a39f90e8ee36de9121c504
Ayushkaushal <ayushk4@gmail.com>
2018-10-18 14:48:12 +0530

    Get ssoToken

--- data/scrapper/login.py
+++ data/scrapper/login.py
@@ -1,35 +1,75 @@
 import requests
-from bs4 import BeautifulSoup
 import getpass
 import time
 
+# Eight cookies have been identified for erp
+session_token = ''# 1. JSESSIONID @ path = /IIT_ERP3 : once you open erp.iitkgp.ac.in/IIT_ERP3 it is assigned
+cookie2 = ''# 2. JSESSIONID @ path = /SSOAdministration : the cookie that identifies you as the user you logged into your account
+ssoToken = ''# 3. ssoToken @ path = / : Use : unknown, seems to be a concatenation of three cookies
+# 4. JSESSIONID @ path = /Establishment : Best guess = It is responsible for the aadhar/voter id details pop up 
+# 5. JSID#/Establishment @ path = /  : Same as the cookie above i.e. 3.
+# 6. JSID#IITERP3 @ path = / : Same as the cookie 1. mentioned above 
+# 7. JSESSIONID @ path = /Acad : Useful for the data obtained from erp, pertaining to acads, like time table, grads.....
+# 8. JSID @path = / : Same as above i.e. 7.
+# Please contribute to the information above
 
-ERP_LOGIN_URL = 'https://erp.iitkgp.ac.in/IIT_ERP3/' #to get the session cookie
+# *************Cookies are named accordingly*********************
+ERP_HOME_URL = 'https://erp.iitkgp.ac.in/IIT_ERP3/' #to get the session cookie
 SECURITY_QUESTION_URL = 'https://erp.iitkgp.ac.in/SSOAdministration/getSecurityQues.htm'
+GET_LOGIN_SESSION_URL = 'https://erp.iitkgp.ac.in/SSOAdministration/login.htm?sessionToken={0}&requestedUrl=https://erp.iitkgp.ac.in/IIT_ERP3/home.htm'
+LOGIN_URL = 'https://erp.iitkgp.ac.in/SSOAdministration/auth.htm'
 
+headers = {
+    'timeout': '20',
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/51.0.2704.79 Chrome/51.0.2704.79 Safari/537.36',
+}
 
+s = requests.Session()
 def get_session_cookie ():
-    request_erp = requests.get(ERP_LOGIN_URL)
-    
-    #Following two lines are needed coz erp has freaking two html tags
-    soup = BeautifulSoup(request_erp.text)
-    soup = BeautifulSoup(request_erp.text[len(soup):])
+    print("Getting session cookies")
+    response_erp = s.get(ERP_HOME_URL)
+    session_token = response_erp.cookies['JSESSIONID']
+    response_login = requests.post(GET_LOGIN_SESSION_URL.format(session_token)
+                               , data = {'sessionToken' : session_token
+                                        ,'requestedUrl' : 'https://erp.iitkgp.ac.in/IIT_ERP3/home.htm'
+                                        }
+                                )
+    cookie2 = response_login.cookies['JSESSIONID']
+    return (session_token,cookie2)
 
-    session_tag = soup.find('input',attrs = {'id':'sessionToken'})
-    session_cookie = session_tag['value']
-    return session_cookie
 
-print(get_session_cookie())
-
-def get_security():
+def get_user_details():
     while (1):
         roll_no = input("Enter your roll number : ")
-        r2 = requests.post (SECURITY_QUESTION_URL, data = {'user_id': roll_no})
-        if(r2.text!= 'FALSE'):
+        response_security_question = s.post (SECURITY_QUESTION_URL, data = {'user_id': roll_no},headers=headers)
+        if(response_security_question.text!= 'FALSE'):
             break
         print("Roll number is wrong\nPlease Enter the details again\n")
         time.sleep(1)
 
     password = getpass.getpass("Enter your password : ")
-    security_ans = getpass.getpass("Answer your  security question - " + r2.text + " : ")
+    security_ans = getpass.getpass("Answer your security question - " + response_security_question.text + " : ")
 
+    return roll_no, password, security_ans
+
+def login_into_erp( roll_no, password, security_ans, session_token, cookie2):
+    login_details = {
+                    'user_id': roll_no,
+                    'password': password,
+                    'answer': security_ans,
+                    'sessionToken': session_token,
+                    'requestedUrl': 'https://erp.iitkgp.ac.in/IIT_ERP3',
+                }
+    
+    response_login_auth_htm = s.post(LOGIN_URL
+                                        , data=login_details
+                                        , headers=headers
+                                        )
+
+    sso_token = response_login_auth_htm.history[1].cookies['ssoToken']
+
+    print(ssoToken)
+
+session_token,cookie2 = get_session_cookie()
+roll_no, password, security_ans = get_user_details()
+login_into_erp(roll_no, password, security_ans, session_token, cookie2)
