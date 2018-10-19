77982c8ba231b6ad8f49597754b5db478218f1fa
Kshitij Saraogi <kshitijsaraogi@gmail.com>
2018-10-19 19:11:38 +0530

    [config]: Use PORT variable instead of HERCULES_PORT

--- common/config.go
+++ common/config.go
@@ -3,5 +3,5 @@ package common
 // Config represents configuration environment to running application
 type Config struct {
 	DatabaseURL string `required:"true" desc:"Connection String, for more info see: https://godoc.org/github.com/lib/pq"`
-	Port        string `default:"8080" desc:"Application listen port"`
+	Port        string `envconfig:"PORT" default:"8080" desc:"Application listen port"`
 }
